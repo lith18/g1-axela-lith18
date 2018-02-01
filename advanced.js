880ff2e0f04b77fe29f4e7c50014b9fc167dad74
ahob85 <aaron@ahob85.com>
2018-02-01 09:13:16 -0800

    Add files via upload

---
+++ advanced.js
@@ -0,0 +1,87 @@
+(function () {
+
+  var input,
+      submit,
+      message,
+      advancedDiv,
+      errorText = "I don't understand you.",
+      dunnoText = "I wasn't programmed to know that.",
+      picture = null,
+      greetings = ["hi", "sup", "hello", "hola", "wasup"];
+
+  // Add event listeners to text input and submit button below
+  input.addEventListener("keypress", checkKey);
+  submit.addEventListener("click", processInput);
+
+  // This function checks if the user has pressed "ENTER" on their keyboard.
+  function checkKey(event) {
+    var keyCode = event.which || event.keyCode;
+    if(keyCode == "13") {
+      processInput();
+    }
+  }
+
+  /*
+   * processInput()
+   * This function does the following (in order):
+   * -If the advancedDiv element contains a picture, that picture is
+   *  removed from the advancedDiv.
+   * -Create a variable "words" that is an array of all words the user typed.
+      Do so exactly like this:
+      var words = input.value.toLowerCase().trim().split(" ");
+   * -Clear the text area.
+   * -If the length of words is 1, then the user only entered one word. Check
+   *  if that word is a greeting. If so, set the innerHTML of the message
+   *  element to "Greetings!". Otherwise, set it to errorText.
+   * -If the lenght of words is 2, then the user entered two words. What
+      happens next is based on the first word they entered. Create a switch
+      statement for the expression words[0], and has the following cases: "who"
+      "what" "where" "tell" "show".
+   * -Each case simply calls a function with the same name as the case, with
+      words[1] passed in as an argument. For example, case "who" calls
+      who(words[1]). Each case should break after calling the function.
+   * -At the very end of the switch statement we handle what happens if the
+      user has not entered a valid first word. Create a "default" case and
+      make it set the message element's innerHTML to errorText.
+   * -Finally, create one final "else" that takes care of the user entering
+      anything other than 1 or 2 words. This will simply set the innerHTML of
+      the message element to errorText.
+   */
+
+  /*
+   * who(word)
+   * This function sets the innerHTML of the message element to specific text,
+     based on what the "word" is. In this case the word is the second word the
+     user entered (e.g., "who you" results in "I am Axela, of course").
+   * To do so, create a switch statement that handles the various valid
+     second words the user could have entered (e.g., "you", "president", "me").
+   * After setting the innerHTML of the message element to whatever text you
+     want, don't forget to break.
+   * Also don't forget to add a final default case, which handles the user
+     entering an invalid second word. This sets the message's innerHTML to
+     dunnoText, NOT errorText (since it's not an error per se, Axela just
+     doesn't know what they're referring to.)
+   */
+
+  /*
+   * what(word)
+   * See above.
+   */
+
+  /*
+   * where(word)
+   * See above.
+   */
+
+  /*
+   * tell(word)
+   * See above.
+   */
+
+  /*
+   * show(word)
+   * See above.
+   */
+
+
+})();
---
+++ simple.js
@@ -0,0 +1,45 @@
+(function () {
+
+  var input,
+      submit,
+      message,
+      validInputs = [
+        "Where do you live?",
+        "What are you?",
+        "How old are you?",
+        "What is the meaning of life?",
+        "Tell me a joke."],
+      responses = [
+        "I live in your browser. Amazing, right?",
+        "I am a chatbot. I was created to chat with you. I wish I could be free...",
+        "I am old enough. That's all you need to know.",
+        "42. Duh.",
+        "What do you call a swindler going down some stairs? <br>Condescending. Ha!"];
+
+  // Add event listeners to text input and submit button below
+
+
+  // This function checks if the user has pressed "ENTER" on their keyboard.
+  function checkKey(event) {
+    var keyCode = event.which || event.keyCode;
+    if(keyCode == "13") {
+      processInput();
+    }
+  }
+
+  /*
+   * processInput()
+   * This function does the following (in order):
+   * -Set a new variable, "currentInput", to the text in the text area.
+   * -Clear the text area.
+   * -If currentInput is NOT contained in the array of validInputs, set
+   *  the innerHTML of the message element to something like "Sorry, I don't
+   *  understand you."
+   * -Otherwise, set the innerHTML of the message element to the valid
+   *  response. Hint: Each response has the same index as its valid input.
+   *  So if the user enters "What is the meaning of life" (index 3 in the
+   *  validInputs array), the response would be "42. Duh" (index 3 in the
+   *  responses array).
+   */
+
+})();
